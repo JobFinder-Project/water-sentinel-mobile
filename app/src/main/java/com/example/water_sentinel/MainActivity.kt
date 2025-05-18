44fe1855d075870e1582c14a41a32f8350639765
Peu <jvggaldino11@gmail.com>
2025-05-17 21:16:54 -0400

    add welcome

--- app/src/main/java/com/example/water_sentinel/MainActivity.kt
+++ app/src/main/java/com/example/water_sentinel/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.water_sentinel
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+//import android.content.Intent
+//import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+// androidx.core.view.ViewCompat
+//import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button = findViewById<Button>(R.id.btnEnter)
+        button.setOnClickListener {
+            //val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
         }
     }
 }
