0837adeecdf3f8ce7346f39d9db8fd081bf70619
Peu <jvggaldino11@gmail.com>
2025-05-19 14:46:27 -0400

    add dashboard

--- app/src/main/java/com/example/water_sentinel/WelcomeActivity.kt
+++ app/src/main/java/com/example/water_sentinel/WelcomeActivity.kt
@@ -2,20 +2,20 @@ package com.example.water_sentinel
 
 import android.os.Bundle
 import android.widget.Button
-//import android.content.Intent
+import android.content.Intent
 //import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 // androidx.core.view.ViewCompat
 //import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome)
 
         val button = findViewById<Button>(R.id.btnEnter)
         button.setOnClickListener {
-            //val intent = Intent(this, DashboardActivity::class.java)
+            val intent = Intent(this, DashboardActivity::class.java)
             startActivity(intent)
         }
     }
