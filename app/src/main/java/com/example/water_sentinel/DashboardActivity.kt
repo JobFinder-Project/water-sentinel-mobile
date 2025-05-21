93027baf551ffcb07a693faf62e258ec99710db9
JoaoCarlos22 <jcarlosguimaraes13@gmail.com>
2025-05-20 21:58:30 -0400

    Tela dashboard carregando temperatura

--- app/src/main/java/com/example/water_sentinel/DashboardActivity.kt
+++ app/src/main/java/com/example/water_sentinel/DashboardActivity.kt
@@ -1,20 +1,44 @@
 package com.example.water_sentinel
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
+import com.google.firebase.database.getValue
+import com.google.firebase.Firebase
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dashboard)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val txtTemp = findViewById<TextView>(R.id.tv_temperature)
+        getTemp(txtTemp)
+    }
+
+    // Função que acessa a temperatura no Firebase
+    fun getTemp(txtTemp: TextView) {
+
+        // Configuração do Banco de Dados
+        val database = Firebase.database
+        val myRef = database.getReference("sensor/dht/temperatura1")
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            // Busca o dado toda vez que for alterado
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val temperatura = dataSnapshot.getValue<Float>()
+                txtTemp.text = "$temperatura°C"
+
+            }
+            // Função que trata algum erro
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("Firebase", "Erro ao ler dados", error.toException())
+            }
+        })
     }
 }
