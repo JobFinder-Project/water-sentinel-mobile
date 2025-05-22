aaee65562e3650dc6ad1c8ad657a08d7f3f35815
JoaoCarlos22 <jcarlosguimaraes13@gmail.com>
2025-05-22 19:06:57 -0400

    feat: Identificação de inatividade do embarcado

--- app/src/main/java/com/example/water_sentinel/DashboardActivity.kt
+++ app/src/main/java/com/example/water_sentinel/DashboardActivity.kt
@@ -11,58 +11,119 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 import com.google.firebase.database.getValue
 import com.google.firebase.Firebase
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class DashboardActivity : AppCompatActivity() {
+    private val database = Firebase.database
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dashboard)
-        // Identifica os elementos da interface
-        val txtTemp = findViewById<TextView>(R.id.tv_temperature)
-        getTemp(txtTemp)
-
-        val txtUmi = findViewById<TextView>(R.id.tv_humidity)
-        getUmi(txtUmi)
+        setupFirebaseListener()
     }
 
-    // Função que acessa a temperatura no Firebase
-    fun getTemp(txtTemp: TextView) {
+    private fun setupFirebaseListener() {
+        setupDataListener()
+        setupTimestampListener()
+    }
 
-        // Declara o caminho do dado
-        val database = Firebase.database
-        val myRefTemp = database.getReference("sensor/dht/temperatura")
+    // Função que acessa os dados no Firebase
+    private fun setupDataListener() {
+        // define os elementos UI
+        val txtTemp = findViewById<TextView>(R.id.tv_temperature)
+        val txtUmi = findViewById<TextView>(R.id.tv_humidity)
+        val txtPressao = findViewById<TextView>(R.id.tv_pressure)
 
-        myRefTemp.addValueEventListener(object : ValueEventListener {
-            // Busca o dado toda vez que for alterado
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val temperatura = dataSnapshot.getValue<Int>()
+        // Declara o caminho dos dados do sensor DHT
+        val refDht = database.getReference("sensor/dht/")
+
+        refDht.addValueEventListener(object : ValueEventListener {
+            // Busca temperatura e umidade toda vez que for alterado
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val temperatura = snapshot.child("temperatura").getValue<Int>()
                 txtTemp.text = "$temperatura°C"
 
-            }
-            // Função que trata algum erro
-            override fun onCancelled(error: DatabaseError) {
-                Log.e("Firebase", "Erro ao ler dados", error.toException())
-            }
-        })}
-
-    // Função que acessa a umidade no Firebase
-    fun getUmi(txtUmi: TextView) {
-
-        // Declara o caminho do dado
-        val database = Firebase.database
-        val myRefUmi = database.getReference("sensor/dht/umidade")
-
-        myRefUmi.addValueEventListener(object : ValueEventListener {
-            // Busca o dado toda vez que for alterado
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val umidade = dataSnapshot.getValue<Float>()
+                val umidade = snapshot.child("umidade").getValue<Int>()
                 txtUmi.text = "$umidade%"
+
             }
             // Função que trata algum erro
             override fun onCancelled(error: DatabaseError) {
-                Log.e("Firebase", "Erro ao ler dados", error.toException())
+                Log.e("Firebase", "Erro ao ler dados do sensor DHT", error.toException())
             }
         })
+
+        val refPressao = database.getReference("sensor/bpm180/pressao")
+
+        refPressao.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val pressao = snapshot.getValue<Int>()
+                txtPressao.text = "$pressao Pas"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("Firebase", "Erro ao ler a pressão", error.toException())
+            }
+        })
+    }
+
+    // Função para alterar satus do sistema
+    private fun setupTimestampListener() {
+
+        // define o elemento UI
+        val txtStatus = findViewById<TextView>(R.id.tv_weather_desc)
+
+        // declara o caminho do timestamp
+        val refTimestamp = database.getReference("timestamp/")
+
+        refTimestamp.addValueEventListener(object : ValueEventListener {
+            // acessa o ultimo timestamp
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val horaStr = snapshot.child("hora").getValue<String>()
+                val dataStr = snapshot.child("data").getValue<String>()
+
+                // verifica se os dados não são nulos para formatá-los
+                if (dataStr != null && horaStr != null) {
+                    val formatterData = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                    val formatterHora = DateTimeFormatter.ofPattern("HH:mm:ss")
+
+                    // formata a data e hora registrada no firebase
+                    try {
+                        val data = LocalDate.parse(dataStr, formatterData)
+                        val hora = LocalTime.parse(horaStr, formatterHora)
+                        val dataHora = LocalDateTime.of(data, hora)
+                        val ultTimestamp = dataHora.atZone(java.time.ZoneId.systemDefault()).toInstant().toEpochMilli()
+                        checkStatus(txtStatus, ultTimestamp)
+                    } catch (e: Exception) {
+                        Log.e("DateTime", "Erro ao parsear data/hora", e)
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("Firebase", "Erro ao ler hora", error.toException())
+            }
+        })
+    }
+
+    // função que altera o status do sistema
+    private fun checkStatus(txtStatus: TextView, ultTimestamp: Long) {
+        // captura o timestamp atual
+        val atualTimestamp = System.currentTimeMillis()
+
+        // verifica a diferença de tempo
+        val diferenca = atualTimestamp - ultTimestamp
+        val diferencaSeg = diferenca/1000
+
+        // se caso tiver inatividade a mais de 20 segundos, altera o status
+        if (diferencaSeg > 20) {
+            txtStatus.text = "Sistema inativo"
+        } else {
+            txtStatus.text = "Sistema ativo"
+        }
     }
 
         //val button = findViewById<Button>(R.id.btnMap)
