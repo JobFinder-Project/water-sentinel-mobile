ce0d5dc89223d13296dcf86c2d6ae026d7ef54c6
Peu <jvggaldino11@gmail.com>
2025-05-22 10:20:37 -0400

    add: splash screens

--- app/src/main/java/com/example/water_sentinel/WelcomeActivity.kt
+++ app/src/main/java/com/example/water_sentinel/WelcomeActivity.kt
@@ -3,6 +3,8 @@ package com.example.water_sentinel
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 //import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 // androidx.core.view.ViewCompat
@@ -13,10 +15,10 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        val button = findViewById<Button>(R.id.btnEnter)
-        button.setOnClickListener {
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, DashboardActivity::class.java)
             startActivity(intent)
-        }
+            finish()
+        }, 3000)
     }
 }
