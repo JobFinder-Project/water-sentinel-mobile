a90fd1fc574353aea713f15e2f7feceafc39d3ed
JoaoCarlos22 <jcarlosguimaraes13@gmail.com>
2025-05-23 10:47:53 -0400

    raw: Realização da solicitação de permissão de notificação

--- app/src/main/java/com/example/water_sentinel/DashboardActivity.kt
+++ app/src/main/java/com/example/water_sentinel/DashboardActivity.kt
@@ -1,10 +1,18 @@
 package com.example.water_sentinel
 
+import android.Manifest
+import android.app.AlertDialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -16,21 +24,32 @@ import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
+
 class DashboardActivity : AppCompatActivity() {
+    companion object {
+        private const val CODIGO_PERMISSAO_NOTIFICACAO = 1001
+    }
     private val database = Firebase.database
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dashboard)
+
+        criarCanalNotificacao()
+
+        solicitarPermissaoNotificacao()
+
         setupFirebaseListener()
     }
 
+    // Função que recupera os dados do Firebase
     private fun setupFirebaseListener() {
         setupDataListener()
         setupTimestampListener()
     }
 
-    // Função que acessa os dados no Firebase
+    // Função que acessa os dados do Firebase
     private fun setupDataListener() {
         // define os elementos UI
         val txtTemp = findViewById<TextView>(R.id.tv_temperature)
@@ -125,6 +144,89 @@ class DashboardActivity : AppCompatActivity() {
             txtStatus.text = "Sistema ativo"
         }
     }
+
+    private fun criarCanalNotificacao() {
+        val name = getString(R.string.channel_name)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val mChannel = NotificationChannel("water-sentinel-channel", name, importance).apply {
+            enableVibration(true)
+            vibrationPattern = longArrayOf(100, 200, 100, 200)
+        }
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(mChannel)
+    }
+
+    private fun solicitarPermissaoNotificacao() {
+        val permissao = Manifest.permission.POST_NOTIFICATIONS
+        when {
+
+            // caso a permissão já tenha sido concedida
+            ContextCompat.checkSelfPermission(
+                this,
+                permissao
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // Permissão já concedida
+                //showNotification()
+            }
+
+            // caso o usuário já tenha negado antes
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this, permissao) -> {
+                // explicar ao usuário porque a permissão é necessária
+                mostrarExplicacaoPermissao()
+            }
+
+            // primeira vez solicitando
+            else -> {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(permissao),
+                    CODIGO_PERMISSAO_NOTIFICACAO)
+            }
+        }
+    }
+
+    // Função da caixa de diálogo da permissão
+    private fun mostrarExplicacaoPermissao() {
+        AlertDialog.Builder(this)
+            .setTitle("Permissão de Notificações")
+            .setMessage("Este app precisa enviar notificações para alertar sobre mudanças no sistema de monitoramento de água.")
+            .setPositiveButton("Permitir") { _, _ ->
+                solicitarPermissaoNotificacao()
+            }
+            .setNegativeButton("Agora não", null)
+            .show()
+    }
+
+    // Função para tratar a resposta da solicitação de permissao
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            CODIGO_PERMISSAO_NOTIFICACAO -> {
+                // se a requisição for cancelada, o array estará vazio
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permissão concedida
+                    Toast.makeText(
+                        this,
+                        "Notificações ativadas",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    // Permissão negada
+                    Toast.makeText(
+                        this,
+                        "Notificações desativadas",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+
 
         //val button = findViewById<Button>(R.id.btnMap)
         //button.setOnClickListener {
