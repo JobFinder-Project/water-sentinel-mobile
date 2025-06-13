919f46f0b6d2495dc01c7378f8686940bbcb7136
JoaoCarlos22 <jcarlosguimaraes13@gmail.com>
2025-06-13 15:18:44 -0400

    :recycle: refactor: dados do posto de alerta com visibilidade global

--- app/src/main/java/com/example/water_sentinel/MyApp.kt
+++ app/src/main/java/com/example/water_sentinel/MyApp.kt
@@ -1,11 +1,39 @@
 package com.example.water_sentinel
 
 import android.app.Application
+import com.google.android.gms.maps.model.LatLng
 
 class MyApp : Application() {
-    var globalStatusRisco: Int = 4
+    companion object {
+        lateinit var instance: MyApp
+            private set
+    }
+
+    //var globalStatusRisco: Int = 4
+    //var postos: List<PostoAlerta> = emptyList()
+    var postoAlerta: PostoAlerta = PostoAlerta()
 
     override fun onCreate() {
         super.onCreate()
+        instance = this
     }
+
+}
+data class PostoAlerta(
+    var nome: String = "Posto 1",
+    var latLng: LatLng = LatLng(-3.14162, -58.43252),
+    var status: Int = -1,
+    var riscoPorcentagem: Int = 0,
+    var umidade: Int = 0,
+    var temperatura: Float = 0f,
+    var pressao: Int = 0,
+    var ultimaAtualizacao: String = "",
+    var endereco: Endereco = Endereco("Raimundo Garcia Gama", "Jauary", "Itacoatiara", "Amazonas")
+) {
+    data class Endereco(
+        val rua: String,
+        val bairro: String,
+        val cidade: String,
+        val estado: String
+    )
 }
