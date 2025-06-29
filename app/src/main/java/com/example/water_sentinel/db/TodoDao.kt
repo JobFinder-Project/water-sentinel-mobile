1908f02be5028023d8fb8f1ec4ef745dec86d22e
Peu <jvggaldino11@gmail.com>
2025-06-29 07:35:59 -0400

    :bug: fix: rename class DataHistory

--- app/src/main/java/com/example/water_sentinel/db/TodoDao.kt
+++ app/src/main/java/com/example/water_sentinel/db/TodoDao.kt
@@ -7,10 +7,10 @@ import androidx.room.Query
 @Dao
 interface TodoDao {
     @Insert
-    suspend fun insert(history: dataHistory)
+    suspend fun insert(history: DataHistory)
 
     @Query("SELECT * FROM data_history WHERE type = :type ORDER BY timestamp DESC LIMIT 5")
-    suspend fun getLatestFiveByType(type: String): List<dataHistory>
+    suspend fun getLatestFiveByType(type: String): List<DataHistory>
 
 
 }
